8ef579dd3d8462e25c32357c079b43bffd243cd9
Your Name <you@example.com>
2022-08-22 03:16:01 +0300

    Added expand

--- main.py
+++ main.py
@@ -71,22 +71,22 @@ save_button = Button(top_frame, text="Save", command=saving)
 save_button.pack(side=LEFT)
 
 context_listbox = Listbox(filters_frame)
-context_listbox.pack(side=TOP, fill=Y)
+context_listbox.pack(side=TOP, fill=BOTH, expand=True)
 
 project_listbox = Listbox(filters_frame)
-project_listbox.pack(side=BOTTOM, fill=Y)
+project_listbox.pack(side=BOTTOM, fill=BOTH, expand=True)
 
 task_listbox = Listbox(tasks_frame, height=20, width=70)
 task_listbox.insert(END, "some_task from @test_context in +test_project")
-task_listbox.pack(fill=BOTH)
+task_listbox.pack(fill=BOTH, expand=True)
 
 status_label = Label(status_frame, text="Idle", bd=1, relief=SUNKEN, anchor=W)
 status_label.pack(side=BOTTOM, fill=X)
 
 top_frame.pack(side=TOP, fill=X)
 status_frame.pack(side=BOTTOM, fill=X)  # must be before others!
-filters_frame.pack(side=LEFT, fill=Y)
-tasks_frame.pack(side=RIGHT, fill=BOTH)
+filters_frame.pack(side=LEFT, fill=BOTH, expand=True)
+tasks_frame.pack(side=RIGHT, fill=BOTH, expand=True)
 
 root.after(0, loading)
 root.mainloop()
